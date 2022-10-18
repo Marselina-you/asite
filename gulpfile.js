45e3ee477679d3c719a19e03bfbe2669bebaa3a6
unknown <marselina88@list.ru>
2022-10-18 20:05:25 +0300

    svg

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+// Основной модуль
+import gulp from "gulp";
+// Импорт путей
+import { path } from "./gulp/config/path.js";
+import { plugins } from "./gulp/config/plugins.js";
+
+global.app = {
+    path: path,
+	gulp: gulp,
+    plugins: plugins
+}
+
+//импорт задач
+import { copy } from "./gulp/tasks/copy.js";
+import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
+//наблюдатель за изменениями в файлах
+function  watcher() {
+	gulp.watch(path.watch.files, copy);
+    gulp.watch(path.watch.html, html);
+	
+	
+}
+const mainTasks = gulp.parallel(copy, html);
+//построение сценариев выполнения задач
+const dev = gulp.series(reset, mainTasks,  watcher);
+//выполнение сценария по умолчанию
+gulp.task('default', dev);
